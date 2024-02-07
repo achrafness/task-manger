f44b0d6f7db4031e286b76ecc846294286a4635e
achrafness <achraf.nessighaoui13@gmail.com>
2024-02-07 17:22:31 +0100

    add main

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,8 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDB = (url) =>{
-    return mongoose
-    .connect(url,{
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    })
-}
-module.exports = connectDB
-    // .then(()=>console.log("mongo db is connect"))
-    // .catch((err)=>console.log(err))
+const connectDB = (url) => {
+  return mongoose.connect(url);
+};
+module.exports = connectDB;
+// .then(()=>console.log("mongo db is connect"))
+// .catch((err)=>console.log(err))
