f7b7bfb6a59288f75ad014edc1a752c6c72d91f3
achrafness <achraf.nessighaoui13@gmail.com>
2024-02-07 16:58:10 +0100

    add main

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const app = express();
+
 const tasks = require("./routes/tasks")
+
 const connectDB = require("./db/connect")
 require("dotenv").config()
+
 const notFound = require("./middleware/not-found")
 const errorhandler = require("./middleware/erreur")
 
 app.use(express.static("./public"))
 
 // middleware 
-app.use(express.urlencoded({extends :false}))
 app.use(express.json())
 
 app.use("/api/v1/tasks",tasks)
